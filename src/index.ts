65c59fdfa2dfaaf898ec457240f6f2189a2ab64c
Tony Walmsley <tony@aosd.co.uk>
2022-01-26 13:13:05 +0000

    Allow env selection in `test/index.html`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { CompanyApiOptions, createCompanyApi } from './CompanyApi';
+import { createClient } from 'num-client';
 
-export { CompanyApiOptions, createCompanyApi };
+export { CompanyApiOptions, createCompanyApi, createClient };
